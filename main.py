eff71575c0ff411d3e15bbd89bc8f8c222717258
poko1 <you@example.com>
2023-07-01 12:46:51 -0700

    Train and save a random forest model to predict survival rate

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+df = pd.read_csv("titanic.csv")
+X = df[['pclass', 'sex', 'fare']]
+X = pd.get_dummies(X)
+y = df['survived']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test =  train_test_split(X,y,test_size=0.2,random_state=1)
+
+from sklearn.ensemble import RandomForestClassifier
+randf = RandomForestClassifier()
+randf.fit(X_train,y_train)
+pred = randf.predict(X_test)
+print(classification_report(y_test,pred))
+
+import pickle
+pickle.dump(randf,open('survival_pred.pkl','wb'))
